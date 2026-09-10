d9cbbdc8cbe53894906300d3b06ca86ec8b702bf
OEBLab-TAMU <jeongho2000@gmail.com>
2026-09-10 13:36:07 -0500

    Update news.js

--- news.js
+++ news.js
@@ -1,7 +1,7 @@
 const newsItems = [
   {
     date:" September 2025", tag:"Grant",
-    title:"Development of Virtual Reality-Based Commercial Fishing Safety Training Program",
+    title:"From Laboratory to Deck: A Randomized Controlled Trial of Back-Support Exoskeletons to Reduce Musculoskeletal Injury Risk in Commercial Fishing",
     desc:"This NIOSH-funded randomized controlled trial (RCT) will evaluate whether back-support exoskeletons can reduce physical strain, musculoskeletal pain, and related health outcomes during real-world fishing operations.",
     img:"images/logo.png"},
   {
